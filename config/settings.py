b1810cf0188598401af4ca1dc7c32f50abfa5f21
eliakim <ngouakouassieliakim2@gmail.com>
2026-07-19 03:29:49 +0000

    Correctif CSRF_TRUSTED_ORIGINS pour HTTPS

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,7 @@ DEBUG = os.getenv("DEBUG", "True") == "True"
 INTERNAL_IPS = ["127.0.0.1"]
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",") if os.getenv("CSRF_TRUSTED_ORIGINS") else []
 
 
 # Application definition
